cef5c7e920ba47ce000e5937674cf576880e85af
super.orien <congacon025@gmail.com>
2023-12-03 20:28:10 +0700

    add build project 4

---
+++ setup/terraform/versions.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_version = "1.3.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
